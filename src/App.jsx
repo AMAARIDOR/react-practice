c0cb5b52cc95b3a2c01641b1d1f7a27672ba7794
Amaari <arish2k3@gmail.com>
2023-04-21 16:25:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+function Avatar({ person, height }) {
+    return (
+        <div>
+            The the word's greatest person is {person.name}, {person.age} years
+            old and he is {height}cm tall
+        </div>
+    );
+}
+
 function App() {
-    return <>Hello world</>;
+    return <Avatar person={{ name: "Amaari", age: 20 }} height={176} />;
 }
 
 export default App;
