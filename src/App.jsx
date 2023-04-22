9703c2cc872171c6836a7703aa4a6b2ec12b101c
amaari <arish2k3@gmail.com>
2023-04-22 13:46:12 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-const people = [{
-    id: 1,
-    name: 'Creola Katherine Johnson',
-    profession: 'mathematician',
-  }, {
-    id: 2,
-    name: 'Mario José Molina-Pasquel Henríquez',
-    profession: 'chemist',
-  }, {
-    id: 3,
-    name: 'Mohammad Abdus Salam',
-    profession: 'physicist',
-  }, {
-    id: 4,
-    name: 'Percy Lavon Julian',
-    profession: 'chemist',  
-  }, {
-    id: 5,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-}];
-
 function App() {
-    const chemists = people.filter(person => person.profession === "chemist")
-    
-    const listItems = chemists.map(person => <li key={person.id}> {person.name} {person.profession} </li>)
+  return (
+    <Toolbar
+      onPlayMovie={() => alert("Playing movie")}
+      onPlayMusic={() => alert("Playing music")}
+    />
+  );
+}
 
-    return (
-        <>
-            <ul>
-                {listItems}
-            </ul>
-        </>
-    )
+function Toolbar({ onPlayMovie, onPlayMusic }) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>Play Movie</Button>
+      <Button onClick={onPlayMusic}>Play Music</Button>
+    </div>
+  );
+}
+
+function Button({ onClick, children }) {
+  return <button onClick={onClick}> {children}</button>;
 }
 
 export default App;
