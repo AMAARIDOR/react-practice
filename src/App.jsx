d9d008118c29cca02de11cd94a21895ca77936ed
Amaari <arish2k3@gmail.com>
2023-04-25 20:19:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function Button({ onSmash, children }) {
-  return <button onClick={onSmash}>{children}</button>;
-}
-
-function PlayButton({ movieName }) {
-  function handleClick() {
-    alert(`Playing movie ${movieName}`);
-  }
-
-  return <Button onSmash={handleClick}>Play {movieName}</Button>;
-}
-
-function UploadButton() {
-  return <Button onSmash={() => alert("Uploading!")}>Upload Image</Button>;
-}
-
-export default function ToolBar() {
-  return (
-    <>
-      <PlayButton movieName="Kiki do you love me!?" />
-      <UploadButton />
-    </>
-  );
+export default function App() {
+  return <>Hello World</>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Toolbar from "./App";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Toolbar />
+    <App />
   </React.StrictMode>
 );
