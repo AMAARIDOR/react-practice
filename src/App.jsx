b69e3f0d38dac46ea8ce0020a5c8d6ec3dcce743
Amaari <arish2k3@gmail.com>
2023-04-25 21:49:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 export default function App() {
-  return <>Hello World</>;
+	return (
+		<div
+			className="Toolbar"
+			onClick={() => {
+				alert("You clicked on the this toolbar!");
+			}}
+		>
+			<button onClick={() => alert("Playing")}>Play movie</button>
+			<button onClick={() => alert("Upoading")}>Upload Image</button>
+		</div>
+	);
 }
