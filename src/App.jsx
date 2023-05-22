f82ceecae6c1db2e863de74ac5f47536a3d580a2
raregiovanni <giovannirare@gmail.com>
2023-05-22 19:01:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,4 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-export default function App() {
-    return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault();
-                alert("Submitting");
-            }}
-        >
-            <input />
-            <button>Send</button>
-        </form>
-    );
-}
+export default function App() {}
