1ed1494356758835d9653e17cd6081632229a00d
raregiovanni <giovannirare@gmail.com>
2023-05-22 19:15:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
-export default function App() {}
+function Input() {
+    const [message, setMessage] = useState("");
+    const [update, setUpdated] = useState(message);
+
+    const handleChange = (e) => setMessage(e.target.value);
+    const handleClick = () => setUpdated(message);
+
+    return (
+        <div className="inputContainer">
+            <input type="text" onChange={handleChange} value={message} />
+            <button onClick={handleClick}>Add Item +</button>
+            <div>{message}</div>
+            <div>{update}</div>
+        </div>
+    );
+}
+
+function Item({ value }) {
+    return <div className="itemContainer">{value}</div>;
+}
+
+export default function App() {
+    return (
+        <div className="appContainer">
+            <Item />
+            <Input />
+        </div>
+    );
+}
